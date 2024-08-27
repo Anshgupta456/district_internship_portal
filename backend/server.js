ef182695f6967e230a1d372cb73603de33b88f16
Akatsuki-Uchiha-Itachi <raviarya4542@gmail.com>
2024-08-28 00:50:29 +0530

    fix profile

--- backend/server.js
+++ backend/server.js
@@ -15,18 +15,7 @@ const corsOptions = {
 
 // Middleware
 app.use(cors());  // Apply CORS middleware with options
-app.use(function(req,res,next){
-  res.header("Access-Control-Allow-Origin","*");
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET,HEAD,OPTIONS,POST,PUT,DELETE"
-  );
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept,Authorization"
-  );
-  next();
-})
+
 app.use(bodyParser.json());
 
 // Routes
