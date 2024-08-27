5405b41369429fd7312993c3c589ca25d896d955
Akatsuki-Uchiha-Itachi <raviarya4542@gmail.com>
2024-08-27 13:35:54 +0530

    cors fixed

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,19 @@ const corsOptions = {
 };
 
 // Middleware
-app.use(cors(corsOptions));  // Apply CORS middleware with options
+app.use(cors());  // Apply CORS middleware with options
+app.use(function(req,res,next){
+  res.header("Access-Control-Allow-Origin","*");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,OPTIONS,POST,PUT,DELETE"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept,Authorization"
+  );
+  next();
+})
 app.use(bodyParser.json());
 
 // Routes
